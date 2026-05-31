0ebeacc33918e4bb796b2c800c9dae96adb473d7
Sven Haardiek <sven@haardiek.de>
2026-05-31 20:08:39 +0200

    fix moves

--- src/chessmove.rs
+++ src/chessmove.rs
@@ -35,11 +35,19 @@ impl Move {
         promotion: Option<PieceType>,
     ) -> Self {
         let mut m = Self::NULL;
-        m.set_from(from);
-        m.set_to(to);
-        m.set_move_type(move_type);
+
+        // set from
+        m.0 |= from as u32;
+
+        // set to
+        m.0 |= (to as u32) << 6;
+
+        // set move type
+        m.0 |= (move_type as u32) << 12;
+
+        // set promotion
         if let Some(pt) = promotion {
-            m.set_promotion(pt);
+            m.0 |= ((pt as u32 - 2) & 0b11) << 14;
         }
         m
     }
@@ -48,39 +56,106 @@ impl Move {
         Square::new((self.0 & 0b111111) as u8)
     }
 
-    pub fn set_from(&mut self, square: Square) {
-        self.0 |= square as u32;
-    }
-
     pub fn to(self) -> Square {
         Square::new((self.0 >> 6 & 0b111111) as u8)
-    }
-
-    pub fn set_to(&mut self, square: Square) {
-        self.0 |= (square as u32) << 6;
     }
 
     pub fn move_type(self) -> MoveType {
         MoveType::new((self.0 >> 12 & 0b11) as u8)
     }
 
-    pub fn set_move_type(&mut self, move_type: MoveType) {
-        self.0 |= (move_type as u32) << 12;
+    pub fn promotion(self) -> Option<PieceType> {
+        let bits = (self.0 >> 14 & 0b11) as u8;
+        if self.move_type() == MoveType::Promotion {
+            Some(PieceType::new(bits + 2))
+        } else {
+            None
+        }
     }
-
-    pub fn promotion(self) -> PieceType {
-        PieceType::new(((self.0 >> 14 & 0b11) + 1) as u8)
-    }
-
-    pub fn set_promotion(&mut self, piece_type: PieceType) {
-        self.0 |= ((piece_type as u32 - 1) & 0b11) << 14;
-    }
-
     pub fn score(self) -> u16 {
         (self.0 >> 16) as u16
     }
 
     pub fn set_score(&mut self, score: u16) {
-        self.0 |= (score as u32) << 16
+        self.0 = (self.0 & !0xFFFF0000) | ((score as u32) << 16);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::piece::PieceType;
+    use crate::square::Square;
+
+    #[test]
+    fn test_move_normal() {
+        let m = Move::new(Square::E2, Square::E4, MoveType::Normal, None);
+        assert_eq!(m.from(), Square::E2);
+        assert_eq!(m.to(), Square::E4);
+        assert_eq!(m.move_type(), MoveType::Normal);
+        assert_eq!(m.promotion(), None);
+        assert_eq!(m.score(), 0);
+    }
+
+    #[test]
+    fn test_move_promotion() {
+        let m = Move::new(
+            Square::E7,
+            Square::E8,
+            MoveType::Promotion,
+            Some(PieceType::Queen),
+        );
+        assert_eq!(m.from(), Square::E7);
+        assert_eq!(m.to(), Square::E8);
+        assert_eq!(m.move_type(), MoveType::Promotion);
+        assert_eq!(m.promotion(), Some(PieceType::Queen));
+    }
+
+    #[test]
+    fn test_move_promotion_all_pieces() {
+        for pt in [
+            PieceType::Knight,
+            PieceType::Bishop,
+            PieceType::Rook,
+            PieceType::Queen,
+        ] {
+            let m = Move::new(Square::A7, Square::A8, MoveType::Promotion, Some(pt));
+            assert_eq!(m.promotion(), Some(pt));
+        }
+    }
+
+    #[test]
+    fn test_move_en_passant() {
+        let m = Move::new(Square::E5, Square::D6, MoveType::EnPassant, None);
+        assert_eq!(m.from(), Square::E5);
+        assert_eq!(m.to(), Square::D6);
+        assert_eq!(m.move_type(), MoveType::EnPassant);
+        assert_eq!(m.promotion(), None);
+    }
+
+    #[test]
+    fn test_move_castling() {
+        let m = Move::new(Square::E1, Square::G1, MoveType::Castling, None);
+        assert_eq!(m.from(), Square::E1);
+        assert_eq!(m.to(), Square::G1);
+        assert_eq!(m.move_type(), MoveType::Castling);
+    }
+
+    #[test]
+    fn test_move_score() {
+        let mut m = Move::new(Square::E2, Square::E4, MoveType::Normal, None);
+        assert_eq!(m.score(), 0);
+        m.set_score(1000);
+        assert_eq!(m.score(), 1000);
+        m.set_score(u16::MAX);
+        assert_eq!(m.score(), u16::MAX);
+    }
+
+    #[test]
+    fn test_null_move() {
+        assert_eq!(Move::NULL.from(), Square::A1);
+        assert_eq!(Move::NULL.to(), Square::A1);
+        assert_eq!(Move::NULL.move_type(), MoveType::Normal);
+        assert_eq!(Move::NULL.score(), 0);
     }
 }
