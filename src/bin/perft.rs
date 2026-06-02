78161d1eded4c37dddc403b3dcdd229b8df73c66
Sven Haardiek <sven@haardiek.de>
2026-06-02 21:19:47 +0200

    perft candidate, now debugging starts...

---
+++ src/bin/perft.rs
@@ -0,0 +1,38 @@
+use std::env;
+
+use angelika::{chessmovelist::MoveList, position::Position};
+
+fn perft(pos: &mut Position, depth: u32) -> u64 {
+    if depth == 0 {
+        return 1;
+    }
+
+    let mut nodes: u64 = 0;
+
+    let mut moves = MoveList::default();
+    pos.generate_moves(&mut moves);
+    for idx in 1..moves.len() {
+        let mut new_pos = *pos;
+        new_pos.make_move(moves.get(idx));
+        if new_pos.is_check() {
+            continue;
+        }
+        nodes += perft(&mut new_pos, depth - 1)
+    }
+    nodes
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        eprint!("Usage: {} <fen> <depth>", args[0]);
+        return;
+    }
+
+    let fen: &str = args[1].as_str();
+    let depth: u32 = args[2].parse().expect("depth should be number");
+
+    let mut pos = Position::from_fen(fen).expect("unable to parse fen string");
+    print!("{}", perft(&mut pos, depth))
+}
